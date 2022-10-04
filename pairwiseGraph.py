534ce1c3416047289b066ba784157284ffc5c590
Damien-Garcia-Bioinformatics <damien.garcia@laposte.net>
2022-10-04 16:46:04 +0200

    Modifications of how command line parameters, and how the results are stored. Now, datetime is used to store all results if no job name is provided.

--- pairwiseGraph.py
+++ pairwiseGraph.py
@@ -3,13 +3,15 @@
 # Script Programming Project
 # GARCIA Damien, M2BB
 
-# Please consider reading the README file for explanation of the code.
+# Please consider taking a look at my GitHub repository for in depth explanation of the code :
+# https://github.com/Damien-Garcia-Bioinformatics/pairwiseGraph
 
 
 ### Library imports ###
 
 import os
 import sys
+import time
 from Bio import pairwise2
 from Bio.pairwise2 import format_alignment
 from random import randint, choice
@@ -19,16 +21,17 @@ from matplotlib import pyplot
 
 ### Functions ###
 
-def help() :
-	if os.path.exists("asciiArt") :
+def help(ask4help) :
+	if os.path.exists("asciiArt") and ask4help == False :
 		print(easter_egg())
-	print('Error : pairwiseGraph.py execution error : Fasta file not found.')
-	print('\nSyntax is : ./pairwiseGraph [optional arguments] ')
+		print('[pairwiseGraph] [Error] : pairwiseGraph.py execution error : Fasta file not found.\n')
+	
+	print('Syntax is : ./pairwiseGraph [optional arguments] ')
+	print('\tjobName   : set name for the directory containing all results')
 	print('\tsequences : path to fasta file containing sequences.')
-	print('\tgraphName : set file name for the graph representation (example.png)')
 	print('\tsaveAlign : save all alignment files in a dedicated repertory "align" (-yes or -no)')
-	print('\ttreshold  : set a treshold score for edges to visualize on graph (float)')
-	print('\nGo to : https://github.com/Damien-Garcia-Bioinformatics/pairwiseGraph for more informations.')
+	print('\tthreshold : set a threshold score for edges to visualize on graph (float)')
+	print('\nFor more informations, please take a look at my GitHub repository :\nhttps://github.com/Damien-Garcia-Bioinformatics/pairwiseGraph')
 	exit(1)
 
 def easter_egg() :
@@ -39,7 +42,11 @@ def easter_egg() :
 			asciiString += line
 	return asciiString
 
-# Extracts parameters from a plain text file and returns a dictionary
+# Returns current time which is used for file naming purpose
+def get_time() :
+	return time.strftime('%y%m%d_%H%M%S')
+
+# Extracts parameters from a plain text file (scriptParam.txt) and returns a dictionary
 def read_param_file() :
 	dicParam = {}
 	with open("scriptParam.txt") as paramFile :
@@ -51,6 +58,15 @@ def read_param_file() :
 				if words[0] == "file" :
 					dicParam["file"] = words[1]
 					continue
+				elif words[0] == "job_name" and not (words[1] == 'None' or words[1] == '') :
+					dicParam["job_name"] = words[1]
+					continue
+				elif words[0] == "job_name" :
+					if words[1] in ['None',''] :
+						dicParam['job_name'] = get_time()
+						continue
+					dicParam['job_name'] = words[1]
+					continue
 				elif words[0] == "number_of_sequences" :
 					dicParam["number_of_sequences"] = int(words[1])
 					continue
@@ -60,26 +76,20 @@ def read_param_file() :
 				elif words[0] == "maximum_length" :
 					dicParam["maximum_length"] = int(words[1])
 					continue
-				elif words[0] == "filename" :
-					dicParam["filename"] = words[1]
-					continue
 				elif words[0] == "save_alignment_files" :
 					if words[1] == 'Yes' :
 						dicParam["save_alignment_files"] = True
 						continue
 					dicParam["save_alignment_files"] = False
 					continue
-				elif words[0] == "graph_name" :
-					dicParam["graph_name"] = words[1]
-					continue
-				elif words[0] == "treshold" :
-					dicParam["treshold"] = float(words[1])
+				elif words[0] == "threshold" :
+					dicParam["threshold"] = float(words[1])
 					continue
 	return dicParam
 
 
 # Generates a fasta file containing nucleic acid sequences
-def fasta_gen(filename, nbSeq, minLen, maxLen) :
+def fasta_gen(jobName, nbSeq, minLen, maxLen) :
 	tablebase = 'ATCG'
 	sequences = []
 
@@ -91,7 +101,7 @@ def fasta_gen(filename, nbSeq, minLen, maxLen) :
 		sequences.append(seq)
 
 	# Writing of generated sequences in fasta file
-	with open(filename, 'w+') as file :
+	with open(f"results/{jobName}/seqGen.fasta", 'w+') as file :
 		for i in range(len(sequences)) :
 			file.write(f">random{i}\n{sequences[i]}\n")
 
@@ -103,7 +113,7 @@ def GCcontent(seq) :
 	for i in range(seqLen) :
 		if seq[i] in ['G','C'] :
 			count += 1
-	return ((count/seqLen)*100)
+	return (count/seqLen)*100
 
 
 # Reads a fasta file and creates a dictionary containing ID, sequence, and GC percentage of every entry
@@ -124,15 +134,15 @@ def read_fasta(file) :
 
 
 # Prints alignment results in a plain text format file. Every alignment is systematicaly named and stored in 'align' directory
-def beautiful_print(result, seq1, seq2) :
+def beautiful_print(jobName, result, seq1, seq2) :
 	maxSize = 80
 	p = len(result[0]) // maxSize
 	r = len(result[0]) % maxSize
 	if r != 0 :
 		p += 1
-	if not os.path.exists("align") :
-		os.makedirs("align")
-	with open(f"align/{seq1}_{seq2}.align", "w+") as f :
+	
+	os.makedirs(f"results/{jobName}/align", exist_ok=True)
+	with open(f"results/{dicParam['job_name']}/align/{seq1}_{seq2}.txt", "w+") as f :
 		f.write(f"{seq1} - {seq2} result alignment :\n\n")
 		for i in range(p) :
 			f.write(f"{result[0][i*maxSize:(i+1)*maxSize]}\n")
@@ -141,17 +151,22 @@ def beautiful_print(result, seq1, seq2) :
 		f.write(result[3])
 
 
-# Generates RGBA color format considering weight and treshold parameters.
-def colors_RGBA(weight, treshold) :
-	R = min((weight*(1/treshold)-(2*treshold)), 1)
+# Generates RGBA color format considering weight and threshold parameters.
+def colors_RGBA(weight, threshold) :
+	R = min((weight*(1/threshold)-(2*threshold)), 1)
 	G = 0
 	B = 1 - R
 	A = R
 	return [R,G,B,A]
 
+# def write_csv() :
+# 	with open(f"align_{jobname}.csv") as f :
+# 		f.write(f"{seq1};{seq2};{score};{GCcontent1};{GCcontent2}\n")
+# 	return 0
+
 
 # Generates a graph visualization of pairwise sequence alignments.
-def graph_gen(dicFasta, treshold, saveAlign, graphName) :
+def graph_gen(dicFasta, threshold, saveAlign, jobName) :
 	lstID = list(dicFasta) # List of nodes name
 	
 	# Creates edges weighted by percentage of alignments between sequences
@@ -170,7 +185,7 @@ def graph_gen(dicFasta, treshold, saveAlign, graphName) :
 				if saveAlign :
 					for alignment in alignments :
 						result = (pairwise2.format_alignment(*alignment)).split('\n')
-						beautiful_print(result, lstID[i], lstID[j])
+						beautiful_print(jobName, result, lstID[i], lstID[j])
 				
 				# List containing length of longest sequence between the two sequences used for pairwise alignment
 				seqLen.append(max(len(dicFasta[lstID[i]]['sequence']), len(dicFasta[lstID[j]]['sequence'])))
@@ -186,13 +201,12 @@ def graph_gen(dicFasta, treshold, saveAlign, graphName) :
 	edgesAlpha = {}
 	for i in range(len(edges2draw)) :
 		weights.append((edges2draw[i][2]/seqLen[i])) #Poids entre 0 et 1
-		edgesColor[edges2draw[i][0], edges2draw[i][1]] = colors_RGBA(weights[i], treshold)
-		if edgesColor[edges2draw[i][0],edges2draw[i][1]][3] < 0 :		# Case where weight < treshold
+		edgesColor[edges2draw[i][0], edges2draw[i][1]] = colors_RGBA(weights[i], threshold)
+		if edgesColor[edges2draw[i][0],edges2draw[i][1]][3] < 0 :		# Case where weight < threshold
 			edgesColor[edges2draw[i][0],edges2draw[i][1]] = [0,0,0,0]
 			edgesAlpha[edges2draw[i][0],edges2draw[i][1]] = 0 
-		else :															# Case where weight >= treshold
+		else :															# Case where weight >= threshold
 			edgesAlpha[edges2draw[i][0],edges2draw[i][1]] = 0.8
-
 
 	# Graph generation
 	Graph(
@@ -208,47 +222,59 @@ def graph_gen(dicFasta, treshold, saveAlign, graphName) :
 		node_label_fontdict=dict(size=9),
 		node_label_offset=0.14)
 	pyplot.tight_layout()
-	pyplot.savefig(graphName, format='png')
+	pyplot.savefig(f"results/{dicParam['job_name']}/graph.png", format='png')
 	#pyplot.show()
 
 
 ### Script execution ###
 
 if __name__ == '__main__' :
+
+	if len(sys.argv) == 2 and sys.argv[1].lower() == 'help' :
+		ask4help = True
+		help(ask4help)
+	ask4help = False
+
+	if not os.path.exists('results/') :
+		os.makedirs('results/')
+
 	# Extraction of parameters from "script_parameters.txt"
 	print("[pairwiseGraph] Reading script parameters")
 	dicParam = {}
-	dicParam['save_alignment_files'] = False
 	dicParam = read_param_file()
 
 	# Extraction of command line parameters : Parameters from command line have a higher priority which means that they will replace the ones set in scriptParam.txt.
 	if len(sys.argv) >= 2 :
 		for i in range(len(sys.argv)-1) :
+			if sys.argv[i] == "jobName" :
+				dicParam['job_name'] = sys.argv[i+1]
+				continue
 			if sys.argv[i] == "sequences" :
 				dicParam['file'] = sys.argv[i+1]
 				continue
 			if sys.argv[i] == "saveAlign" and (sys.argv[i+1].upper() == "-YES" or sys.argv[i+1].upper() == "-Y") :
 				dicParam['save_alignment_files'] = True
 				continue
-			if sys.argv[i] == "graphName" :
-				dicParam['graph_name'] = sys.argv[i+1]
+			if sys.argv[i] == "threshold" :
+				dicParam['threshold'] = float(sys.argv[i+1])
 				continue
-			if sys.argv[i] == "treshold" :
-				dicParam['treshold'] = float(sys.argv[i+1])
-				continue
+	
+	# Creation of repertory to job results
+	#print(dicParam['job_name'])
+	os.makedirs(f"results/{dicParam['job_name']}/", exist_ok=True)
 
 	# If a fasta file is given in script parameter file
-	if not dicParam["file"] == "None" and os.path.exists(dicParam["file"]) :
+	if dicParam["file"] not in ['None',''] and os.path.exists(dicParam["file"]) :
 		print("[pairwiseGraph] Extraction of data from fasta file")
 		dicFasta = read_fasta(dicParam["file"])
 	# If no fasta file is provided
-	elif dicParam["file"] == "None" :
+	elif dicParam["file"] in ['None',''] :
 		print("[pairwiseGraph] Generating fasta sequences")
-		fasta_gen(dicParam['filename'], dicParam['number_of_sequences'], dicParam['minimum_length'], dicParam['maximum_length'])
-		dicFasta = read_fasta(dicParam['filename'])
+		fasta_gen(dicParam['job_name'], dicParam['number_of_sequences'], dicParam['minimum_length'], dicParam['maximum_length'])
+		dicFasta = read_fasta(f"results/{dicParam['job_name']}/seqGen.fasta")
 	else :
-		help()
+		help(ask4help)
 
 	# Creation and visualization of graph
 	print("[pairwiseGraph] Generating graph file")
-	graph_gen(dicFasta, dicParam['treshold'], dicParam['save_alignment_files'], dicParam['graph_name'])
+	graph_gen(dicFasta, dicParam['threshold'], dicParam['save_alignment_files'], dicParam['job_name'])
